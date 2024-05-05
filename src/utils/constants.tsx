440120bc4f91a27a60fece3f60782147e32d4309
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2024-05-05 18:32:29 +0530

    bug fix

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -5,5 +5,5 @@ export const PAGE = {
 }
 
 export const ROLES = [{label :'Frontend', value: 'frontend'}, {label: 'Backend', value: 'backend'}, {label: 'IOS', value: 'ios'}, {label: 'Android', value: 'android'}, {label: 'Tech Lead', value: 'tech lead'}];
-export const LOCATION = [{label :'Remote', value: 'remote'}, {label :'Mumbai', value: 'mumbai'}, {label: 'Delhi', value: 'delhi'}, {label: 'Bangalore', value: 'bangalore'}, {label :'Chennai', value: 'chennai'}];
+export const LOCATION = [{label :'Remote', value: 'remote'}, {label :'Mumbai', value: 'mumbai'}, {label: 'Delhi Ncr', value: 'delhi ncr'}, {label: 'Bangalore', value: 'bangalore'}, {label :'Chennai', value: 'chennai'}];
 export const MIN_BASE_PAY = [{label: '0L', value: '0'}, {label: '10L', value: '10'}, {label: '20L', value: '20'}, {label: '30L', value: '30'}, {label: '40L', value: '40'}, {label: '50L', value: '50'}, {label: '60L', value: '60'}, {label: '70L', value: '70'}];
